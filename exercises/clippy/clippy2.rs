8af275196f60f5aedbeb1e2a88edecfbc2264723
kcwqxx <3437515542@qq.com>
2024-10-05 14:19:44 +0800

    update

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,11 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+//报错原因：option不是可以迭代的集合，不可以用for循环在option中迭代
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    if let Some(x) =option {
         res += x;
     }
     println!("{}", res);
--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,28 +4,31 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+use std::mem;//使用swap记得加上
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        my_option.unwrap();
+        println!("my_option is None!");//None无法解包
     }
 
     let my_arr = &[
-        -1, -2, -3
+        -1, -2, -3,
         -4, -5, -6
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
-
+    //let my_empty_vec让返回值给了my_empty_vec，但my_empty_vec是let_，没有实际用途
+    vec![1, 2, 3, 4, 5].resize(0, 5);
+    println!("This Vec is empty, see? {:?}", ());//空元组防止报错
+    
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
+   /*  let temp = value_a;
     value_a = value_b;
-    value_b = value_a;
+    value_b = temp;太繁琐，clippy建议用系统自带的swap*/
+    mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
