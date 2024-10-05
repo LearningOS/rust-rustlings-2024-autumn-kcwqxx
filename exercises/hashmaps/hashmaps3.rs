5f0ace314519cc8a2bc34caea74c6ec6333bedfb
kcwqxx <3437515542@qq.com>
2024-10-05 17:04:23 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -28,6 +28,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
+    
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
@@ -39,7 +40,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team_1= scores.entry(team_1_name.clone()).or_insert(Team {goals_scored:0,goals_conceded:0});
+        team_1.goals_scored += team_1_score;//增加进球数
+        team_1.goals_conceded += team_2_score;//增加失球数
+        let team_2= scores.entry(team_2_name.clone()).or_insert(Team {goals_scored:0,goals_conceded:0});
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
+
+
+
     }
+
+
+
     scores
 }
 
